233e2d165223cc3c5e904ac0836e09b1a61319bf
Rachel Amos <rachelamos35@gmail.com>
2021-03-21 15:18:12 -0500

    functioning tables in mysql workbench

--- server.sql
+++ server.sql
@@ -5,23 +5,26 @@ CREATE DATABASE emp_trackerDB;
 USE emp_trackerDB;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY
-    title VARCHAR(30)
-    salary DECIMAL
-    department_id INT
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(12) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY
-    first_name VARCHAR(30)
-    last_name VARCHAR(30)
-    role_id INT
-    manager_id INT
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
 );
 
 INSERT INTO department (name)
@@ -31,4 +34,4 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Salesperson", 80000, 1), ("Sales Lead", 120000, 1), ("Software Engineer", 115000, 2), ("Lead Engineer", 160000, 2), ("Lawyer", 160000, 3), ("Legal Team Lead", 210000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Rachel", "Amos", 1), ("Payton", "Whinnery", 2), ("Caroline", "Miller", 3), ("Jackie", "Hodges", 4), ("Molly", "Gilbert", 5), ("Walter", "Perry", 6), ("Natalie", "Guidry", 7), ("Arwen", "Amos", 8), ("Sarah", "Bishop", 9), ("Jordan", "Watson", 10);
+VALUES ("Rachel", "Amos", 3, 1), ("Payton", "Whinnery", 3, 1), ("Caroline", "Miller", 1, 2), ("Jackie", "Hodges", 2, NULL), ("Molly", "Gilbert", 5, 3), ("Walter", "Perry", 5, 3), ("Natalie", "Guidry", 4, NULL), ("Arwen", "Amos", 1, 2), ("Sarah", "Bishop", 5, 3), ("Jordan", "Watson", 6, NULL);
