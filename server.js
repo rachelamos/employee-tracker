9a64350cfdfb0d40c065f11f00b1e1b9a5991200
Rachel Amos <rachelamos35@gmail.com>
2021-03-21 16:03:11 -0500

    initial inquirer prompt set up

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+// Dependencies
 const mysql = require('mysql');
+const inquirer = require('inquirer');
 
+// Creating Connection to Server
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -8,8 +11,27 @@ const connection = mysql.createConnection({
   database: 'emp_trackerDB',
 });
 
+// Inquirer Prompts
+const start = () => {
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'commands',
+      choices: ['View All Employees', 'View All Departments', 'View All Roles', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role'],
+    }
+  ])
+  .then((answer) => {
+      console.log("Success!");
+      // switch case for each choice - find in Great Bay
+        connection.end();
+
+    })
+};
+// Connecting to MySQL Server & SQL Database
 connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}`);
-  connection.end();
+  start();
 });
