4261030c1cfd72ab84af7f36926d678a9f49bd3b
Rachel Amos <rachelamos35@gmail.com>
2021-03-21 14:56:35 -0500

    password wiring + some seed data for db

--- server.sql
+++ server.sql
@@ -23,3 +23,12 @@ CREATE TABLE employee (
     role_id INT
     manager_id INT
 );
+
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1), ("Sales Lead", 120000, 1), ("Software Engineer", 115000, 2), ("Lead Engineer", 160000, 2), ("Lawyer", 160000, 3), ("Legal Team Lead", 210000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Rachel", "Amos", 1), ("Payton", "Whinnery", 2), ("Caroline", "Miller", 3), ("Jackie", "Hodges", 4), ("Molly", "Gilbert", 5), ("Walter", "Perry", 6), ("Natalie", "Guidry", 7), ("Arwen", "Amos", 8), ("Sarah", "Bishop", 9), ("Jordan", "Watson", 10);
